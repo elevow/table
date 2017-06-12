c4a005e35a55413708beeb2b0d1aa8da929c83ad
elevow <gene@xtuple.com>
2017-06-11 23:13:07 -0400

    Adding lint & editor configs.  Made changes based on linter but issues on IDE Restart

--- src/js/client.js
+++ src/js/client.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
-import About from "./pages/about";
-import Homepage from "./pages/homepage";
-import Settings from "./pages/settings";
-import Setup from "./pages/setup";
+import About from './pages/about';
+import Homepage from './pages/homepage';
+import Settings from './pages/settings';
+import Setup from './pages/setup';
 
 const app = document.getElementById('app');
 
 export default class Client extends React.Component {
-        render(){
-            return(
-            <Router history={createBrowserHistory()}>
-            <Switch>
-            <Route exact={true} path='/' render={() => (
-                <Homepage />
-            )}/>
-            <Route path='/about' component={About}/>
-            <Route path='/settings' component={Settings}/>
-            <Route path='/setup' component={Setup}/>
-            </Switch>
-            </Router>
-            );
-        }
+  render() {
+    return (
+      <Router history={createBrowserHistory()}>
+        <Switch>
+          <Route exact={true}
+            path='/'
+            render={() => (
+              <Homepage />
+            )}
+          />
+          <Route path='/about' component={About} />
+          <Route path='/settings' component={Settings} />
+          <Route path='/setup' component={Setup} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 ReactDOM.render(<Client />, app);
--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export default class Footer extends React.Component {
-    render() {
-        return (
-<div class="container">
-    <footer>Awesome Site &copy; 2017</footer>
-</div>
-        );
-    }
+  render() {
+    return (
+      <div class="container">
+        <footer>Awesome Site &copy; 2017</footer>
+      </div>
+    );
+  }
 }

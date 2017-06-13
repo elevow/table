0e816c5ce870c60ed95cc0e1acd3d3ac448c16aa
elevow <gene@xtuple.com>
2017-06-12 22:17:22 -0400

    updated randomInt to accept no value and error on same value plus code coverage testing

--- src/js/utils/math.js
+++ src/js/utils/math.js
@@ -1,11 +1,14 @@
 
 function randomInt (max, min) {
     if (max === undefined) {
-        console.error('one value is required: randomInt(max, min)');
-        return null;
+        return Math.floor((Math.random() * Number.MAX_SAFE_INTEGER) + 0);
     }
     if (min === undefined) {
         min = 0;
+    }
+    if ((max - min) <= 0) {
+        console.error('Min cannot equal or greater than Max');
+        return null;
     }
     return Math.floor(Math.random() * (max - min) + min);
 }
--- test/utils/math.test.js
+++ test/utils/math.test.js
@@ -5,22 +5,16 @@ import {
 
 describe('math.js', () => {
     describe('randomInt function', () => {
-        it('will return exact value - 0', () => {
+        it('will return null if values are same', () => {
             const actualValue = math.randomInt(0, 0);
             // console.log('actualValue', actualValue);
-            expect(actualValue).equal(0);
+            expect(actualValue).equal(null);
         });
 
-        it('will return exact value - 0 - no min value', () => {
+        it('will return null if max is 0 - no min value', () => {
             const actualValue = math.randomInt(0);
             // console.log('actualValue', actualValue);
-            expect(actualValue).equal(0);
-        });
-
-        it('will return exact value - 1', () => {
-            const actualValue = math.randomInt(1, 1);
-            // console.log('actualValue', actualValue);
-            expect(actualValue).equal(1);
+            expect(actualValue).equal(null);
         });
 
         it('will not equal the high value', () => {
@@ -29,6 +23,7 @@ describe('math.js', () => {
             const actualValue = math.randomInt(maxValue, minValue);
             // console.log('actualValue', actualValue);
             expect(actualValue).not.equal(maxValue);
+            expect(actualValue).to.be.below(maxValue);
         });
 
         it('will not equal the high value - no min value', () => {
@@ -36,12 +31,13 @@ describe('math.js', () => {
             const actualValue = math.randomInt(maxValue);
             // console.log('actualValue', actualValue);
             expect(actualValue).not.equal(maxValue);
+            expect(actualValue).to.be.below(maxValue);
         });
 
-        it('will return null when no value is provided', () => {
+        it('will return random number when no value is provided', () => {
             const actualValue = math.randomInt();
             // console.log('actualValue', actualValue);
-            expect(actualValue).equal(null);
+            expect(actualValue).to.be.above(0);
         });
     });
 
